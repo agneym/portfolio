3210fd29c310cdd0514d16497642e646f1d9e644
BoyWithSilverWings <agney@outlook.in>
2019-01-19 22:16:54 +0530

    correct naming schemes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/Layout'
+import SEO from '../components/Seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    
   </Layout>
 )
 
