e71de2246d985124776c7b9e78881e891fb700ff
Boy with Silver Wings <agney@outlook.in>
2019-05-09 01:14:18 +0530

    Remove noscript
    New gatsby comes with this by default

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-
-exports.onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents([
-    <noscript key="noscript">
-      Sorry! This site requires JavaScript to be enabled.
-    </noscript>,
-  ]);
-};
