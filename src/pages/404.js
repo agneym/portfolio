51361347ffe1a073bff388cd95bd2b504556c2cc
Boy with Silver Wings <agney@outlook.in>
2019-06-28 20:06:47 +0530

    add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+const Container = styled.main`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1
+        css={`
+          color: red;
+        `}
+      >
+        404
+      </h1>
+      <h2>NOT FOUND</h2>
+      <p>This page does not exist.</p>
+      <Link to="/">Home Page</Link>
+    </Container>
   </Layout>
 );
 
