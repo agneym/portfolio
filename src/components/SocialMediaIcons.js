9c73c1a086207351ce2d2d651fca7cfada23b07c
Agney <agney.menon@bigbinary.com>
2020-02-29 01:09:22 +0530

    add introsection mdx

---
+++ src/components/SocialMediaIcons.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { GitHub, Twitter } from "react-feather";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  margin: 6rem 0;
+`;
+
+const ExternalLink = styled.a`
+  display: inline-block;
+  margin: 0 1rem;
+  text-decoration: none;
+  color: inherit;
+`;
+
+function SocialMediaIcons() {
+  const iconSize = 40;
+  return (
+    <Container>
+      <ExternalLink href="https://github.com/agneym" target="_blank">
+        <GitHub size={iconSize} />
+      </ExternalLink>
+      <ExternalLink href="https://twitter.com/agneymenon" target="_blank">
+        <Twitter size={iconSize} />
+      </ExternalLink>
+    </Container>
+  );
+}
+
+export default SocialMediaIcons;
