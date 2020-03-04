a6e59f7cc2ec5ff3e961b13277dc390c2d400ed3
Agney <agney.menon@bigbinary.com>
2020-03-05 00:34:05 +0530

    change require to relative

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
-          default: require.resolve(`${__dirname}/src/components/Layout.js`),
+          default: require.resolve(`./src/components/Layout.js`),
         },
       },
     },
