99d41a276c561f5d9dde8fa2cac115fd80b51a5c
Agney Menon <agney@circle.co>
2023-03-30 01:49:12 +0530

    setup baseUrl for imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
+import { HeadNav } from "components/HeadNav";
+
 export default function Home() {
   return (
-    <p className="bg-red-100 before:text-xs text-clip w-1 font-heading">Home</p>
+    <div>
+      <HeadNav />
+      <p className="bg-red-100 before:text-xs text-clip w-1 font-heading">
+        Home
+      </p>
+    </div>
   );
 }
---
+++ src/components/HeadNav.js
@@ -0,0 +1,3 @@
+export const HeadNav = () => {
+  return <nav>Header Nav</nav>;
+};
