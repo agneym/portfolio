9f2b1f27fa375c374cce0794d496bbb9073b830e
Agney Menon <agney@circle.co>
2023-03-30 00:07:53 +0530

    remove components

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,3 @@
-import IntroSection from "@/components/IntroSection.mdx";
-import ProjectsList from "@/components/ProjectsList";
-import Description from "@/components/Description";
-import Footer from "@/components/Footer";
-
 export default function Home() {
-  return (
-    <>
-      <IntroSection />
-      <Description />
-      <ProjectsList />
-      <Footer />
-    </>
-  );
+  return <p>Home</p>;
 }
--- src/components/Description.js
+++
@@ -1,5 +0,0 @@
-function Description() {
-  return null;
-}
-
-export default Description;
