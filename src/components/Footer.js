df07cc7d50920f01751eca786bab33beb3aec102
Agney Menon <agney@circle.co>
2023-04-13 02:10:53 +0530

    add svgr

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,26 @@
+import GithubIcon from "images/social-media/github.svg";
+
+const SocialMediaLink = ({ href, iconEl, ariaLabel }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      aria-label={ariaLabel}
+      className="w-7 h-7 text-slate-400 hover:text-slate-300 transition-transform hover:scale-105"
+    >
+      {iconEl}
+    </a>
+  );
+};
+
 export const Footer = () => {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="text-center flex gap-x-2 items-center justify-center">
+      <SocialMediaLink
+        href="https://github.com/agneym"
+        iconEl={<GithubIcon />}
+        ariaLabel="My Github Profile"
+      />
     </footer>
   );
 };
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,12 @@
 export const Intro = () => {
   return (
     <main className="flex items-center justify-center text-center flex-col gap-y-10">
-      <h1 className="text-4xl tracking-tight">Hey 👋</h1>
+      <h1 className="text-4xl tracking-tight">Hey 👋 I'm </h1>
       <div className="flex flex-col gap-y-6">
-        <h1 className="text-4xl tracking-tight">I'm Agney Menon</h1>
-        <p className="text-xl tracking-normal">Web Developer. Storyteller.</p>
+        <h1 className="text-6xl font font-extrabold">Agney Menon</h1>
+        <p className="text-xl tracking-normal text-slate-400">
+          Web Developer. Storyteller.
+        </p>
       </div>
     </main>
   );
