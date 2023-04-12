159042de3bed84fc44ed5d6b2485d409eca8077b
Agney Menon <agney@circle.co>
2023-04-13 01:46:14 +0530

    add a Hi

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,11 @@
 export const Intro = () => {
   return (
-    <main className="flex items-center justify-center text-center flex-col gap-y-4">
-      <h1 className="text-4xl uppercase tracking-wide">Agney Menon</h1>
-      <p>Web Developer. Storyteller.</p>
+    <main className="flex items-center justify-center text-center flex-col gap-y-10">
+      <h1 className="text-4xl tracking-tight">Hey 👋</h1>
+      <div className="flex flex-col gap-y-6">
+        <h1 className="text-4xl tracking-tight">I'm Agney Menon</h1>
+        <p className="text-xl tracking-normal">Web Developer. Storyteller.</p>
+      </div>
     </main>
   );
 };
