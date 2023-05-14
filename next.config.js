9446bb0293aad0d2a79e550482e80bd522564c05
Agney Menon <agney@circle.co>
2023-05-14 23:23:55 +0530

    move to app directory layout

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withSvgr = require("next-plugin-svgr");
 const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPlugins([withSvgr, withMDX], {});
+const nextConfig = withPlugins([withSvgr, withMDX], {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+});
 
 module.exports = nextConfig;
