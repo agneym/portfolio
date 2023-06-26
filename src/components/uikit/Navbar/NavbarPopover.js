728ffcde34d35628d229d65983e84c56a6e547c0
Agney Menon <agney@circle.co>
2023-06-27 00:37:19 +0530

    add popover content styling

--- src/components/uikit/Navbar/NavbarPopover.js
+++ src/components/uikit/Navbar/NavbarPopover.js
@@ -6,11 +6,15 @@ import HamburgerSvg from "images/hamburger.svg";
 export function NavbarPopover({ children }) {
   return (
     <Popover.Root>
-      <Popover.Trigger className="inline-flex md:hidden">
+      <Popover.Trigger className="inline-flex md:hidden" title="Menu">
         <HamburgerSvg width={20} height={20} />
       </Popover.Trigger>
       <Popover.Portal>
-        <Popover.Content sideOffset={5}>{children}</Popover.Content>
+        <Popover.Content sideOffset={24}>
+          <div className="w-[length:var(--radix-popover-content-available-width)] flex flex-col gap-y-6 bg-slate-200/40 dark:bg-slate-900/40 px-4 py-2 shadow-sm backdrop-blur-sm">
+            {children}
+          </div>
+        </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
   );
