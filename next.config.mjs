634fe4a2646c01e9f628de63354007c60d99ab67
Agney Menon <agney@circle.co>
2024-01-17 15:27:31 +0530

    add nextjs redirect for newsletter

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ import withSvgr from "next-plugin-svgr";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  async redirects() {
+    return [
+      {
+        source: "/jem",
+        destination: "https://buttondown.email/agney",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withPlugins(
