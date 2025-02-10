d5ecdb1bfff8ae582ca723f0dcea5ddda901b235
Agney Menon <agney@circle.co>
2025-02-10 10:46:21 +0530

    add eslint recommended rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-export default [
+const eslintConfig = [
   js.configs.recommended,
   ...compat.config({
     extends: ["next/core-web-vitals", "prettier"],
   }),
 ];
+export default eslintConfig;
