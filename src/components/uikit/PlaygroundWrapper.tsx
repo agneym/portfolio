bf24080dcb445e8b8e3363795a73ef58d01261e4
agney <agney@outlook.in>
2025-12-20 21:03:21 +0530

    update typescript types

--- src/components/uikit/PlaygroundWrapper.tsx
+++ src/components/uikit/PlaygroundWrapper.tsx
@@ -2,8 +2,9 @@
 
 import Playground from "@agney/playground";
 import { useTheme } from "next-themes";
+import type { ComponentProps } from "react";
 
-export function PlaygroundWrapper(props) {
+export function PlaygroundWrapper(props: ComponentProps<typeof Playground>) {
   const { theme } = useTheme();
   return (
     <div className="not-prose font-mono text-sm">
