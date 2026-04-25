25ae9b44c488c783846ae2eb6fd235fb7ede59c7
Agney <agney@outlook.in>
2026-04-26 00:43:56 +0530

    fix margin on blog post page

--- src/components/BlogHome/BlogArticleContainer.tsx
+++ src/components/BlogHome/BlogArticleContainer.tsx
@@ -7,7 +7,7 @@ interface BlogArticleContainerProps {
 
 export function BlogArticleContainer({ children }: BlogArticleContainerProps) {
   return (
-    <div className="my-4 grid w-full grid-cols-[minmax(1.5rem,1fr)_minmax(0,65ch)_minmax(1.5rem,1fr)] gap-y-16 lg:my-32">
+    <div className="my-4 grid w-full grid-cols-[minmax(1.5rem,1fr)_minmax(0,65ch)_minmax(1.5rem,1fr)] gap-y-16 lg:mt-16 lg:mb-32">
       <article className="prose-ui col-span-full grid max-w-none grid-cols-[minmax(1.5rem,1fr)_minmax(0,65ch)_minmax(1.5rem,1fr)] [&>*]:col-start-2 [&>*]:min-w-0 [&>.article-full-bleed]:col-span-full [&>.article-full-bleed]:w-full [&>.article-full-bleed]:px-4 md:[&>.article-full-bleed]:px-12 lg:[&>.article-full-bleed]:px-16">
         {children}
       </article>
